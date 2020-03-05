a3c9a5d3c11806627820a8319a4de6d3452ba34b
skachkova-natalia <natalia-skachkova@yandex.ru>
2020-03-05 21:10:16 +0300

    feat: add solution to count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+module.exports = function countCats(matrix) {
+  return [].concat(...matrix).filter(e => e == "^^").length;
 };
