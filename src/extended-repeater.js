d9fe5f8df55ab74f96efa939011c86394dc84f85
skachkova-natalia <natalia-skachkova@yandex.ru>
2020-03-10 11:10:32 +0300

    feat: add solution to extended-repeater, vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    let resultArray = [];
+    let additionalArray = [];
+
+    for (let i = 0; i < (options.additionRepeatTimes || 1); i++) {
+
+        additionalArray.push(options.hasOwnProperty('addition') ? String(options.addition) : '');
+    }
+
+    let additionalString = additionalArray.join(options.additionSeparator || '|');
+
+    for (let i = 0; i < (options.repeatTimes || 1); i++) {
+        resultArray.push(str + additionalString);
+    }
+
+    return resultArray.join(options.separator || '+');
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,68 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(direction = true) {
+        this.direction = direction;
+        this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+        this.VigenereMatrix = this.getSquareMatrix(this.alphabet);
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    getSquareMatrix(alphabet) {
+
+        let matrix = [];
+        for (let i = 0; i < alphabet.length; i++) {
+            matrix[i] = alphabet.slice(i).concat(alphabet.slice(0, i));
+        }
+        return matrix;
+    }
+
+    encrypt(message, key) {
+
+        if (!message || !key)
+            throw new Error;
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let word = '';
+
+        for (let i = 0, j = 0; i < message.length; i++ , j++) {
+
+            if (!this.alphabet.includes(message[i])) {
+                word += message[i];
+                j--;
+                continue;
+            }
+
+            word += this.VigenereMatrix[this.alphabet.indexOf(message[i])][this.alphabet.indexOf(key[j % key.length])];
+
+        }
+
+        return (this.direction) ? word : word.split('').reverse().join('');
+    }
+
+    decrypt(message, key) {
+
+        if (!message || !key)
+            throw new Error;
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let word = "";
+
+        for (let i = 0, j = 0; i < message.length; i++ , j++) {
+
+            if (!this.alphabet.includes(message[i])) {
+                word += message[i];
+                j--;
+                continue;
+            }
+            
+            let row = this.alphabet.indexOf(key[j % key.length])
+            word += this.alphabet[this.VigenereMatrix[row].indexOf(message[i])];
+
+        }
+        return (this.direction) ? word : word.split('').reverse().join('');
     }
 }
 
